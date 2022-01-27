2696962bc71bbe2838e3e5b4c9183693cc0573f3
yashviradia <63194797+yashviradia@users.noreply.github.com>
2022-01-27 14:38:22 +0100

    refactor code and fix bugs

--- notification_manager.py
+++ notification_manager.py
@@ -7,18 +7,21 @@ load_dotenv(find_dotenv())
 MY_EMAIL = os.environ.get("FROM_ADDR")
 PASSWORD = os.environ.get("PASSWORD")
 TO_ADDRS = os.environ.get("TO_ADDRS")
+MAIL_PROVIDER_SMTP_ADDRESS = "smtp.gmail.com"
 
 
 class NotificationManager:
 
-    def send_notification(self, message):
-        connection = smtplib.SMTP("smtp.gmail.com", port=587)
+    def send_emails(self, emails, message, google_flight_link):
+        connection = smtplib.SMTP(MAIL_PROVIDER_SMTP_ADDRESS, port=587)
         connection.starttls()
         connection.login(user=MY_EMAIL, password=PASSWORD)
-        connection.sendmail(
-            from_addr=MY_EMAIL,
-            to_addrs=TO_ADDRS,
-            msg=message
-        )
-        connection.close()
+
+        for email in emails:
+            connection.sendmail(
+                from_addr=MY_EMAIL,
+                to_addrs=email,
+                msg=f"Subject:New Low Price Flight!\n\n{message}\n{google_flight_link}".encode("utf-8")
+            )
+            connection.close()
 
